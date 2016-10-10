2bc906b0ab3f0c5ec77f5ef866aca8c695f3b43e
Jerome Purre <Jerome Purre>
2016-10-10 18:30:13 +0100

    my first commit

--- currencyConverter.groovy
+++ currencyConverter.groovy
@@ -1,6 +1,6 @@
-print "If I give you £1, how many Euro will you give me? ";
+﻿print "If I give you £1, how many Euro will you give me? ";
 double euroOverPoundRatio = Double.parseDouble(System.console().readLine());
-print "If I give you 1€, how many Sterling pounds will you give me? ";
+print "If I give you 1€, how many Sterling Pounds will you give me? ";
 double poundOverEuroRatio = Double.parseDouble(System.console().readLine());
 println "OK, now I know enough.";
 boolean finished = false;
